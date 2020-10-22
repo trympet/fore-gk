85220c5be98c01b22c8ce6afe1bf63294496c6fc
Jørgen Johannesen <jorgen@jrgens-macbook-pro.home>
2020-10-22 15:11:35 +0200

    bugfix

--- scripts/baneguide.js
+++ scripts/baneguide.js
@@ -21,7 +21,7 @@
             if (slide_index > slides.length){slide_index = 1}
             if (slide_index < 1) {slide_index = slides.length}
 
-            for (i = 0; i < slides.length; i++) {
+            for (let i = 0; i < slides.length; i++) {
                 slides[i].style.display = "none";}
             slides[slide_index-1].style.display = 'block';
             console.log(slide_index)
