8e8e468f8de45ac61fb02df47eaee175d6a37881
Trym Lund Flogard <trym@flogard.no>
2020-10-28 15:03:01 +0100

    Forbedre responsivhet

--- scripts/navigasjonsbar.js
+++ scripts/navigasjonsbar.js
@@ -27,124 +27,76 @@ export default class ForeNavigasjon extends HTMLElement {
     },
   ];
 
-  HTML = 
-`<header>
-<img class="hero-img" src="assets/logo.svg" alt="Fore Golfklubb">
+  HTML = `<header>
+<img class="header-img" src="assets/logo.svg" alt="Fore Golfklubb">
 </header>
 <nav>
 <ul id="nav-list">
 </ul>
-<select id="nav-select">
-</select>
+<div class="backdrop"></div>
 </nav>`;
 
+  get backdrop() {
+    return this.shadowRoot.querySelector(".backdrop");
+  }
 
   isActive(path, alias = null) {
-    return window.location.pathname
-      .split("#")[0]
-      .includes(path)
-      || window.location.pathname
-      .split("#")[0]
-      .includes(alias);
+    return (
+      window.location.pathname.split("#")[0].includes(path) ||
+      window.location.pathname.split("#")[0].includes(alias)
+    );
   }
+  
+  template;
+
 
   constructor() {
     super();
-    const template = document.createElement("div");
-    template.innerHTML = this.HTML;
+    this.template = document.createElement("div");
+    this.template.innerHTML = this.HTML;
 
-
-
-    const navList = template.querySelector("#nav-list");
-    const navSelect = template.querySelector("#nav-select");
-    this.SIDER.forEach(({navn, uri, alias}) => {
-      navList.innerHTML += 
-      `<li class="nav-item">
-        <a href="${uri}" class="nav-link ${this.isActive(uri) ? "active" : ""}">${navn}</a>
+    const navList = this.template.querySelector("#nav-list");
+    this.SIDER.forEach(({ navn, uri, alias }) => {
+      navList.innerHTML += `<li class="nav-item ${
+        this.isActive(uri) ? "active" : ""
+      }">
+        <a href="${uri}" class="nav-link">${navn}</a>
       </li>`;
-      navSelect.innerHTML +=
-      `<option value="${uri}" ${this.isActive(uri, alias) ? "selected" : ""}>${navn}</option>
-      `
     });
+    const togglebutton = document.createElement("button");
+    togglebutton.className = "sidenav-button";
 
-    const stiler = document.createElement("style");
-    stiler.textContent = `
-    .hero-img {
-      max-width: 440px;
-      margin: auto;
-      display: block;
-    }
-    #nav-list, #nav-select {
-      font-family: abel, 'open sans';
-      font-size: 1rem;
-    }
-    #nav-select {
-      display: block;
-      user-select: none;
-      cursor: pointer;
-      background: var(--primary-color);
-      width: 100%;
-      padding: .55em .45em;
-      border: none;
-      font-size: 1.25rem;
-      display: none;
-    }
-    .active {
-      font-weight: bold;
-    }
-    #nav-list {
-      display: flex;
-      list-style: none;
-      flex-direction: row;
-      background: var(--primary-color);
-      margin: 0;
-      padding: .5rem 0;
-      line-height: 1;
-    }
-    .nav-item {
-      padding: 8px 16px;
-      border-right: 1px solid rgb(0 0 0 / 50%);
-    }
-    .nav-item:last-of-type {
-      border-right: 0;
-    }
-    .nav-item a {
-      text-decoration: none;
-      color: black;
-    }
-    .nav-item::after {
-      content: '';
-      position: absolute;
-      background: red;
-    }
-    @media only screen and (max-width: 600px) {
-      #nav-list {
-        display: none;
-      }
-      #nav-select {
-        display: block;
-      }
-    }
-    @media only screen and (max-width: 460px) {
-      header {
-        display: none;
-      }
-    }
-    `;
-    template.appendChild(stiler);
+    const toggleItem = document.createElement("li");
+    // navList.innerHTML += `<li class="toggle"><button>-</button></li>`
+    const linkElem = document.createElement("link");
+    linkElem.setAttribute("rel", "stylesheet");
+    linkElem.setAttribute("href", "./css/navigasjon.css");
+    this.template.appendChild(linkElem);
 
+    navList.appendChild(toggleItem);
+    this.attachShadow({ mode: "open" }).appendChild(this.template);
 
-    this.attachShadow({mode: 'open'})
-      .appendChild(template.cloneNode(true));
+    this.backdrop.addEventListener("click", this.onBackdropClick.bind(this))
+    this.shadowRoot.querySelector("nav").append(togglebutton);
     this.shadowRoot
-      .querySelector("#nav-select")
-      .addEventListener("change", this.onSelectonChange);
+      .querySelector("button")
+      .addEventListener("click", this.onNavToggle.bind(this));
 
   }
 
   onSelectonChange(e) {
     window.location.pathname = e.target.value;
   }
+
+  onNavToggle(e) {
+    // forhindrer animasjon når siden lastes inn
+    this.template.querySelector("#nav-list").classList.add("touched")
+    this.template.querySelector("#nav-list").classList.toggle("active")
+  }
+
+  onBackdropClick(e) {
+    this.template.querySelector("#nav-list").classList.remove("active")
+  }
 }
 
-customElements.define('fore-navigasjon', ForeNavigasjon);
+customElements.define("fore-navigasjon", ForeNavigasjon);
