58bcb986920abf96becee27a8f904bb4740b54bb
Trym Lund Flogard <trym@flogard.no>
2020-11-01 19:28:03 +0100

    implementert værmelding og baneforhold

---
+++ scripts/værmelding.js
@@ -0,0 +1,54 @@
+//@ts-check
+
+// Værmeldingen bruker Locationforecast 2.0 av Meteorologisk institutt.
+// Dokumentasjon finnes her https://api.met.no/weatherapi/locationforecast/2.0/documentation
+const endpoint =
+  "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=59.933333&lon=10.716667";
+const ikonPath = "./images/vaermelding";
+const ikonFormat = "svg";
+
+const imageContainer = document.querySelector(".værmelding-ikon");
+const temperaturElement = document.querySelector(".værmelding-temperatur");
+
+const getNowcast = async () => {
+  const res = await (await fetch(endpoint)).json();
+  const timeFrames = res?.properties?.timeseries;
+  if (timeFrames) {
+    const currentVærmelding = timeFrames[0];
+    temperaturElement.textContent = getTemperatur(currentVærmelding);
+    imageContainer["href"].baseVal = getIkonUri(currentVærmelding);
+  }
+};
+
+const getIkonUri = (timeframe) => {
+  const ikon = timeframe?.data["next_1_hours"]?.summary["symbol_code"];
+  return `${ikonPath}/${ikon}.${ikonFormat}`;
+};
+
+const getTemperatur = (timeframe) =>
+  timeframe?.data?.instant?.details?.["air_temperature"] + "°C";
+
+getNowcast();
+
+
+const setBaneforhold = () => {
+  const dag = new Date(Date.now())
+    .getDay();
+  const baneforholdElement = document.querySelector("#baneforhold");
+
+  let forhold = "";
+  switch (dag) {
+    case 1:
+    case 4:
+      forhold = "Nyklipt i dag";
+      break;
+    case 0:
+      forhold = "Nylig vannet";
+      break;
+    default:
+      forhold = "Nylig klipt"
+      break;
+  }
+  baneforholdElement.textContent = forhold;
+}
+setBaneforhold();
