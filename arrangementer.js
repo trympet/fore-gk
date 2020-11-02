27df19bd74b228626b72d1cc78767695ce089af2
Kaja Askeland Gabrielsen <kajaag@stud.ntnu.no>
2020-11-02 10:33:11 +0100

    hei

--- arrangementer.js
+++ arrangementer.js
@@ -7,15 +7,20 @@ for (let i=0; i<dager.length;i++){
         dager[i].classList.add("idag");
     }
     console.log(showPopUp.bind(this, dagTall))
-    dager[i].addEventListener("click", showPopUp.bind(this, dagTall));
+    dager[i].addEventListener("click", showPopUp.bind(this, dagTall, dager[i]));
     
 }
 
 
 const dagerArray = ['Klubbmesterskap!','Ikke program denne dagen!','Ikke program denne dagen!','Dag for damer','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Lekedag for barn!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!','Ikke program denne dagen!'];
 
-function showPopUp(dag, e) {
-        console.log(dag);
-        document.getElementById("text").innerHTML = dagerArray[dag - 1];
+function showPopUp(dag, element, e) {
+    document.getElementById("text").innerHTML = dagerArray[dag - 1];
+    let aktiv = document.getElementsByClassName("aktivDag");
+    for (let i=0;i<aktiv.length;i++){
+        let el = aktiv[i];
+        el.classList.remove("aktivDag");
+    }
+    element.classList.add("aktivDag");
 }
 
