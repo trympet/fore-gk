dcdb4a91d0b9c516c67da70e258b7cbf0a4e1d45
Trym Lund Flogard <trym@flogard.no>
2020-11-04 19:14:42 +0100

    Lagt til kalender.json, iht. oppsett i P2

--- scripts/arrangementer.js
+++ scripts/arrangementer.js
@@ -1,50 +1,22 @@
-let dag = document.getElementsByClassName("dag")[0];
-let dager = dag.children;
-
-for (let i = 0; i < dager.length; i++) {
-  let dagTall = parseInt(dager[i].innerHTML); // dag i måneden
-  if (dagTall === new Date().getDate()) {
-    dager[i].classList.add("idag"); // marker hvilken dag det er
+// IIFE
+(async () => {
+  let dag = document.getElementsByClassName("dag")[0];
+  let dager = dag.children;
+  
+  for (let i = 0; i < dager.length; i++) {
+    let dagTall = parseInt(dager[i].innerHTML); // dag i måneden
+    if (dagTall === new Date().getDate()) {
+      dager[i].classList.add("idag"); // marker hvilken dag det er
+    }
+    dager[i].addEventListener("click", showPopUp.bind(this, dagTall));
   }
-  dager[i].addEventListener("click", showPopUp.bind(this, dagTall));
-}
-
-// array med informasjon om hva som skjer de ulike dagene i måneden.
-const dagerArray = [
-  "Klubbmesterskap! Første start går kl 09.00, oppmøte på gressbanen bak klubbhuset kl 07.30.",
-  "Damedag! Se informasjon lenger ned på siden.",
-  "Senior herre formiddag. Se informasjon lenger ned på siden.",
-  null,
-  null,
-  null,
-  null,
-  null,
-  "Damedag! Se informasjon lenger ned på siden.",
-  "Senior herre formiddag. Se informasjon lenger ned på siden.",
-  "Åpen dag for alle nye golfere. Gratis påmelding.",
-  null,
-  null,
-  null,
-  null,
-  "Damedag! Se informasjon lenger ned på siden.",
-  "Senior herre formiddag. Se informasjon lenger ned på siden.",
-  null,
-  null,
-  null,
-  "Lekedag for barn! Vi deler oss inn i flere puljer, dere vil bli satt opp på en gruppe og få møtetidspunkt etter dere har meldt dere på.",
-  null,
-  "Damedag! Se informasjon lenger ned på siden.",
-  "Senior herre formiddag. Se informasjon lenger ned på siden.",
-  null,
-  null,
-  null,
-  null,
-  null,
-  "Damedag! Se informasjon lenger ned på siden.",
-  "Senior herre formiddag. Se informasjon lenger ned på siden.",
-];
-
-// viser melding når bruker klikker på dato
-function showPopUp(dag, e) {
-  document.getElementById("text").innerHTML = dagerArray[dag - 1] || "Ikke program denne dagen! Banen er åpen som vanlig, sjekk våre åpningstider på hjem-siden.";
-}
+  
+  // array med informasjon om hva som skjer de ulike dagene i måneden.
+  const req = await fetch("./api/kalender.json");
+  const dagerArray = await req.json()
+  
+  // viser melding når bruker klikker på dato
+  function showPopUp(dag, e) {
+    document.getElementById("text").innerHTML = dagerArray[dag - 1] || "Ikke program denne dagen! Banen er åpen som vanlig, sjekk våre åpningstider på hjem-siden.";
+  }
+})()
